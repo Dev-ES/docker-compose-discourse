8d937ec47cd30f4f689df6099675698357c31c50
Juliano Petronetto <juliano.petronetto@gmail.com>
2018-02-02 18:14:37 -0200

    Passing threads via env
    Signed-off-by: Juliano Petronetto <juliano.petronetto@gmail.com>

--- puma.rb
+++ puma.rb
@@ -4,9 +4,12 @@ if ENV['RAILS_ENV'] == 'production'
   APP_ROOT = '/usr/src/app'
   num_workers = ENV["NUM_WEBS"].to_i > 0 ? ENV["NUM_WEBS"].to_i : 2
 
+  min_threads = ENV["MIN_THREADS"].to_i >= 0 ? ENV["MIN_THREADS"].to_i : 4
+  max_threads = ENV["MAX_THREADS"].to_i >= 0 ? ENV["MAX_THREADS"].to_i : 16
+
   # Second, you can choose how many threads that you are going to run at same time.
-  workers "#{num_workers}"
-  threads 8, 32
+  workers = "#{num_workers}"
+  threads = "#{min_threads}", "#{max_threads}"
 
   preload_app!
 
