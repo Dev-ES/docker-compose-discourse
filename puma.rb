f1d24c5765cde32135c8c154afb12ab0331bb80c
Juliano Petronetto <juliano.petronetto@gmail.com>
2018-02-02 17:49:18 -0200

    Fix warnigs and reduce workers
    Signed-off-by: Juliano Petronetto <juliano.petronetto@gmail.com>

--- puma.rb
+++ puma.rb
@@ -2,7 +2,7 @@ if ENV['RAILS_ENV'] == 'production'
 
   # First, you need to change these below to your situation.
   APP_ROOT = '/usr/src/app'
-  num_workers = ENV["NUM_WEBS"].to_i > 0 ? ENV["NUM_WEBS"].to_i : 4
+  num_workers = ENV["NUM_WEBS"].to_i > 0 ? ENV["NUM_WEBS"].to_i : 2
 
   # Second, you can choose how many threads that you are going to run at same time.
   workers "#{num_workers}"
